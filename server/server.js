98bf019fccd102f11f38b57a10c0505c9e54ae44
Filipe <filipedaniel.branco@dci.education>
2023-11-27 15:14:37 +0100

    styled the Hero and Gallery

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.use("/auth", authRoutes);
 
 // Server instance
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on ${PORT} ....😃`);
